2a53e67759dd4e1f7bc355d3e9e07e346af7d842
Michael Uti <utimichael9@gmail.com>
2020-10-29 04:13:18 +0100

    templates: add vuetify support

--- politeia/templates/vue.config.js
+++ politeia/templates/vue.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    pages: {
-        'home': {
-            entry: './src/pages/Home/main.ts',
-            template: 'public/index.html',
-            title: 'Home',
-        },
-
-        'proposal': {
-            entry: './src/pages/Proposal/main.ts',
-            template: 'public/index.html',
-            title: 'Proposals',
-        }
+  "pages": {
+    "home": {
+      "entry": "./src/pages/Home/main.ts",
+      "template": "public/index.html",
+      "title": "Home"
+    },
+    "proposal": {
+      "entry": "./src/pages/Proposal/main.ts",
+      "template": "public/index.html",
+      "title": "Proposals"
     }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
